a3231b268c622c0c7e965a6e3b0de7501ce0e81d
MhdMoh <mhd.nidal.mhd@gmail.com>
2025-07-09 19:51:16 +0200

    Initial Commit

---
+++ ios-github-actions-example/ios_github_actions_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_github_actions_exampleApp.swift
+//  ios-github-actions-example
+//
+//  Created by Mohamad Mohamad on 2025. 07. 09..
+//
+
+import SwiftUI
+
+@main
+struct ios_github_actions_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
